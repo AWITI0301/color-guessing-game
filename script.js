208ee67a3d710e0a388ce5cc5491c59247717ed6
AWITI0301 <johnoketchawiti@gmail.com>
2022-07-05 13:19:15 +0300

    define variables for the buttons

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ var boxes = document.querySelectorAll(".box");
 var s = document.querySelector('.rgbspan');
 var colors = generateRandomColor(6);
 var pickedColor = colors[Math.floor(Math.random() * 6)];
+s.textContent = pickedColor;
+
+var playbtn = document.querySelector("#playAgain");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+var boxCount = 6;
+var statusText = document.querySelector(".status")
