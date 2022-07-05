29fe72913e4aea93a8e17fb1ef2454d28a63cee7
AWITI0301 <johnoketchawiti@gmail.com>
2022-07-05 13:34:07 +0300

    define function of random color

--- script.js
+++ script.js
@@ -107,3 +107,11 @@ function generateRandomColor(num) {
     }
     return arr;
 }
+
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
